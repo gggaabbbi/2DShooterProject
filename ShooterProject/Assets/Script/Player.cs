e090d69833f5b92d9ff0d59a4d51179b4dcd410d
Gabriela Barros <126923137+gggaabbbi@users.noreply.github.com>
2023-08-08 20:56:03 -0300

    Top Down Movement

---
+++ ShooterProject/Assets/Script/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    private Animator animator;
+    private Transform playerTransform;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float movex = Input.GetAxis("Horizontal") * velocity * Time.deltaTime;
+        float movey = Input.GetAxis("Vertical") * velocity * Time.deltaTime;
+        playerTransform.Translate(movex, movey, 0);
+    }
+}
