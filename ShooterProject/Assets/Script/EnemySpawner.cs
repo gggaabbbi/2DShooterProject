14f2e2c9e9d7f0a1be013c057f99d3396e054e14
Gabriela Barros <126923137+gggaabbbi@users.noreply.github.com>
2023-08-14 21:09:10 -0300

    Enemy

---
+++ ShooterProject/Assets/Script/EnemySpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] Vector2 minSpawnPosition, maxSpawnPosition;
+    [SerializeField] GameObject enemyDino;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnEnemies() 
+    {
+        yield return new WaitForSeconds(1f);
+        float xPosition = Random.Range(minSpawnPosition.x, maxSpawnPosition.x);
+        float yPosition = Random.Range(minSpawnPosition.y, maxSpawnPosition.y);
+        Instantiate(enemyDino, new Vector2(xPosition, yPosition),
+            Quaternion.identity);
+    }
+}
--- ShooterProject/Assets/Script/Player.cs
+++ ShooterProject/Assets/Script/Player.cs
@@ -5,12 +5,25 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float velocity;
+    [SerializeField] private float velocity = 10;
+    [SerializeField] private int lives = 3;
     private Animator animator;
     private Transform playerTransform;
 
+    public static Player instance;
+
     private void Awake()
     {
+        //singleton
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
         animator = GetComponent<Animator>();
         playerTransform = GetComponent<Transform>();
     }
@@ -22,8 +35,23 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        float movex = Input.GetAxis("Horizontal") * velocity * Time.deltaTime;
-        float movey = Input.GetAxis("Vertical") * velocity * Time.deltaTime;
+        float movex = Input.GetAxisRaw("Horizontal") * velocity * Time.deltaTime;
+        float movey = Input.GetAxisRaw("Vertical") * velocity * Time.deltaTime;
         playerTransform.Translate(movex, movey, 0);
+        print(lives);
+        if(lives <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public Vector3 GetPlayerPosition()
+    {
+        return playerTransform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        lives--;
     }
 }
